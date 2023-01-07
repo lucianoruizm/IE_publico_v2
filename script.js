8ce5129b29ffdc1bc497dc61732470abaef70117
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-01-06 21:42:24 -0300

    new object

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+let formLength  = document.getElementById('data-form').length
+
 const capturar = () => {
     class Info{
         constructor(id, date, hour, ingreso, egreso){
@@ -13,23 +15,31 @@ const capturar = () => {
         
     }
 
-    let id = document.querySelector('#empresa-select').value
     let date = document.querySelector('#date').value
+
+    let id = document.querySelector('#empresa-select').value
     let hour = document.querySelector('.hour').value
     let entryCount = document.querySelector('.entry').value
     let exitCount = document.querySelector('.exit').value
 
-    const data = new Info(id, date, hour, entryCount, exitCount)
-    
-    console.log(data);
+    let id2 = document.querySelector('#empresa-select').value
+    let hour2 = document.querySelector('.hour2').value
+    let entryCount2 = document.querySelector('.entry2').value
+    let exitCount2 = document.querySelector('.exit2').value
 
-    addNewObject(data)
+    const data = new Info(id, date, hour, entryCount, exitCount)
+    const data2 = new Info(id2, date, hour2, entryCount2, exitCount2)
+    
+    
+    console.log(data, data2);
+
+    addNewObject(data, data2)
     
 }
 
 let db = []
 
-const addNewObject = (data) => {
-    db.push(data)
+const addNewObject = (data, data2) => {
+    db.push(data, data2)
     console.log(db); 
 }
