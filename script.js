cabbea4024bcbb57c28722498466a48ff7d0d501
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-01-08 20:10:59 -0300

    ...

--- script.js
+++ script.js
@@ -5,19 +5,17 @@ const capturar = () => {
     class Info{
         constructor(id, date, hour, ingreso, egreso){
             this.id = id;
-            this.date = date;
-            this.hour = hour;
+            this.date = date + '' + hour;
             this.ingreso = ingreso;
             this.egreso = egreso;
-            // this.totalIngresos = totalIngresos;
             // this.totalEgresos = totalEgresos;
         }
         
     }
 
     let date = document.querySelector('#date').value
-
     let id = document.querySelector('#empresa-select').value
+
     let hour = document.querySelector('.hour').value
     let entryCount = document.querySelector('.entry').value
     let exitCount = document.querySelector('.exit').value
@@ -27,13 +25,21 @@ const capturar = () => {
     let entryCount2 = document.querySelector('.entry2').value
     let exitCount2 = document.querySelector('.exit2').value
 
+    
     const data = new Info(id, date, hour, entryCount, exitCount)
     const data2 = new Info(id2, date, hour2, entryCount2, exitCount2)
-    
     
     console.log(data, data2);
 
     addNewObject(data, data2)
+
+    let entries = arrayIteration(document.querySelectorAll('.entries'))
+    let totalEntries = sumArray(entries)
+
+    let exits = arrayIteration(document.querySelectorAll('.exits'))
+    let totalExits = sumArray(exits)
+
+    console.log("Total entradas: ", totalEntries, "Total salidas: ", totalExits);
     
 }
 
@@ -42,4 +48,23 @@ let db = []
 const addNewObject = (...data) => {
     db.push(data)
     console.log(db); 
+}
+
+//FUNCTIONS
+const arrayIteration = (count) => {
+    
+    let array = []
+    for(i=0; i<count.length; i++){
+        array.push(count[i].value)
+    }
+    return array
+
+}
+
+const sumArray = (array) => {
+
+    let total = array.reduce(
+        (accumulator, currentValue) => Number(accumulator) + Number(currentValue), 0)
+
+    return total
 }
