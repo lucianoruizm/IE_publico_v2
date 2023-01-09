3a544f31012715166d4eef0f994642ff91a112be
lucianoruizm <lucianoruizm993@gmail.com>
2023-01-09 13:32:53 -0300

    for objects

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const capturar = () => {
     class Info{
         constructor(id, date, hour, ingreso, egreso){
             this.id = id;
-            this.date = date + '' + hour;
+            this.date = date + ' ' + hour;
             this.ingreso = ingreso;
             this.egreso = egreso;
             // this.totalEgresos = totalEgresos;
@@ -13,25 +13,42 @@ const capturar = () => {
         
     }
 
-    let date = document.querySelector('#date').value
-    let id = document.querySelector('#empresa-select').value
+    for(i = 1; i<=5; i++) {
+        let data = "data" + i
+        let hour = "hour" + i
+        let entryCount = "entryCount" + i
+        let exitCount = "exitCount" + i
 
-    let hour = document.querySelector('.hour').value
-    let entryCount = document.querySelector('.entry').value
-    let exitCount = document.querySelector('.exit').value
+        let date = document.querySelector('#date').value
+        let id = document.querySelector('#empresa-select').value
+    
+        hour = document.querySelector('.hour' + [i]).value
+        entryCount = document.querySelector('.entry' + [i]).value
+        exitCount = document.querySelector('.exit' + [i]).value
 
-    let id2 = document.querySelector('#empresa-select').value
-    let hour2 = document.querySelector('.hour2').value
-    let entryCount2 = document.querySelector('.entry2').value
-    let exitCount2 = document.querySelector('.exit2').value
+        data = new Info(id, date, hour, entryCount, exitCount)
+
+        addNewObject(data)
+    }
+
+    // let date = document.querySelector('#date').value
+    // let id = document.querySelector('#empresa-select').value
+
+    // let hour = document.querySelector('.hour').value
+    // let entryCount = document.querySelector('.entry').value
+    // let exitCount = document.querySelector('.exit').value
+
+    // let id2 = document.querySelector('#empresa-select').value
+    // let hour2 = document.querySelector('.hour2').value
+    // let entryCount2 = document.querySelector('.entry2').value
+    // let exitCount2 = document.querySelector('.exit2').value
 
     
-    const data = new Info(id, date, hour, entryCount, exitCount)
-    const data2 = new Info(id2, date, hour2, entryCount2, exitCount2)
+    // const data = new Info(id, date, hour, entryCount, exitCount)
+    // const data2 = new Info(id2, date, hour2, entryCount2, exitCount2)
     
-    console.log(data, data2);
+    // console.log(data, data2);
 
-    addNewObject(data, data2)
 
     let entries = arrayIteration(document.querySelectorAll('.entries'))
     let totalEntries = sumArray(entries)
@@ -47,8 +64,9 @@ let db = []
 
 const addNewObject = (...data) => {
     db.push(data)
-    console.log(db); 
 }
+
+console.log(db);
 
 //FUNCTIONS
 const arrayIteration = (count) => {
